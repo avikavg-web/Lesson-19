f8c4c927c95d99a321fc88703b681a302d66492c
avikavg-web <avikavg@gmail.com>
2026-02-19 18:00:28 -0800

    Lesson 19

---
+++ Homework.py
@@ -0,0 +1,9 @@
+import math
+
+degree = float(input("Enter the angle in degree: "))
+
+angle = math.radians(degree)
+
+print("Sin: ", math.sin(angle))
+print("Cosin ", math.cos(angle))
+print("Tan: ", math.tan(angle))
